d8563cec6af75cf78a019dc5cc8a0e31f2fe61a9
Giulio Gavardi <giulio.gavardi@eng.it>
2011-05-09 07:47:33 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Studio/trunk@14930 99afaf0d-6903-0410-885a-c66a8bbb5f81

---
+++ it.eng.spagobi.studio.birt/src/it/eng/spagobi/studio/birt/services/WizardLauncher.java
@@ -0,0 +1,31 @@
+package it.eng.spagobi.studio.birt.services;
+
+import it.eng.spagobi.studio.birt.wizards.SpagoBINewBirtReportWizard;
+
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.PlatformUI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WizardLauncher {
+
+	private static Logger logger = LoggerFactory.getLogger(WizardLauncher.class);
+
+	
+	static public void wizardLaunch(){
+		logger.debug("IN");
+		SpagoBINewBirtReportWizard sbindw = new SpagoBINewBirtReportWizard();
+		sbindw.setCalledFromMenu(true);
+
+		// from menu has no selection
+		sbindw.init(PlatformUI.getWorkbench(), null);
+		// Create the wizard dialog
+		WizardDialog dialog = new WizardDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),sbindw);
+		// Open the wizard dialog
+		dialog.open();
+
+		logger.debug("OUT");
+
+	}
+	
+}
