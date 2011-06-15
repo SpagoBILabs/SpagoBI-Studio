9bab96634b7ca400e6181a1688e54ec11008ff37
Giulio Gavardi <giulio.gavardi@eng.it>
2011-06-15 15:14:19 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Studio/trunk@15695 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- it.eng.spagobi.studio.utils/src/it/eng/spagobi/studio/utils/services/SpagoBIServerObjectsFactory.java
+++ it.eng.spagobi.studio.utils/src/it/eng/spagobi/studio/utils/services/SpagoBIServerObjectsFactory.java
@@ -20,43 +20,13 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  **/
 package it.eng.spagobi.studio.utils.services;
 
-import it.eng.spagobi.sdk.datasets.bo.SDKDataSet;
-import it.eng.spagobi.sdk.datasets.bo.SDKDataStoreMetadata;
-import it.eng.spagobi.sdk.datasources.bo.SDKDataSource;
-import it.eng.spagobi.sdk.documents.bo.SDKDocument;
-import it.eng.spagobi.sdk.documents.bo.SDKDocumentParameter;
-import it.eng.spagobi.sdk.documents.bo.SDKFunctionality;
-import it.eng.spagobi.sdk.documents.bo.SDKTemplate;
-import it.eng.spagobi.sdk.engines.bo.SDKEngine;
-import it.eng.spagobi.sdk.exceptions.MissingParameterValue;
-import it.eng.spagobi.sdk.maps.bo.SDKFeature;
-import it.eng.spagobi.sdk.maps.bo.SDKMap;
-import it.eng.spagobi.sdk.proxy.DataSetsSDKServiceProxy;
-import it.eng.spagobi.sdk.proxy.DataSourcesSDKServiceProxy;
-import it.eng.spagobi.sdk.proxy.MapsSDKServiceProxy;
-import it.eng.spagobi.studio.utils.bo.DataSource;
-import it.eng.spagobi.studio.utils.bo.DataStoreMetadata;
-import it.eng.spagobi.studio.utils.bo.Dataset;
-import it.eng.spagobi.studio.utils.bo.Document;
-import it.eng.spagobi.studio.utils.bo.DocumentParameter;
-import it.eng.spagobi.studio.utils.bo.Engine;
-import it.eng.spagobi.studio.utils.bo.Functionality;
-import it.eng.spagobi.studio.utils.bo.GeoFeature;
-import it.eng.spagobi.studio.utils.bo.GeoMap;
 import it.eng.spagobi.studio.utils.bo.Server;
-import it.eng.spagobi.studio.utils.bo.Template;
 import it.eng.spagobi.studio.utils.exceptions.NoActiveServerException;
-import it.eng.spagobi.studio.utils.exceptions.NoServerException;
-import it.eng.spagobi.studio.utils.sdk.SDKProxyFactory;
-import it.eng.spagobi.studio.utils.services.server.ServerHandler;
 import it.eng.spagobi.studio.utils.services.serverobjects.ServerDataSources;
 import it.eng.spagobi.studio.utils.services.serverobjects.ServerDatasets;
 import it.eng.spagobi.studio.utils.services.serverobjects.ServerDocuments;
 import it.eng.spagobi.studio.utils.services.serverobjects.ServerEngines;
 import it.eng.spagobi.studio.utils.services.serverobjects.ServerMaps;
-
-import java.rmi.RemoteException;
-import java.util.Vector;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
