d106592d2212fc6d3e8f7beddc64eb4415ab3e10
chiarelli <chiarelli>
2011-07-11 15:12:52 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Studio/trunk@15912 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- it.eng.spagobi.studio.oda/src/it/eng/spagobi/studio/oda/impl/Driver.java
+++ it.eng.spagobi.studio.oda/src/it/eng/spagobi/studio/oda/impl/Driver.java
@@ -7,6 +7,15 @@
 
 package it.eng.spagobi.studio.oda.impl;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import it.eng.spagobi.services.proxy.DataSetServiceProxy;
+import it.eng.spagobi.studio.oda.impl.runtime.RuntimeConnection;
+import it.eng.spagobi.utilities.engines.EngineConstants;
+
 import org.eclipse.datatools.connectivity.oda.IConnection;
 import org.eclipse.datatools.connectivity.oda.IDriver;
 import org.eclipse.datatools.connectivity.oda.LogConfiguration;
@@ -23,14 +32,152 @@ public class Driver implements IDriver
 {
     static String ODA_DATA_SOURCE_ID = "spagobi.birt.oda";  //$NON-NLS-1$
     
+    public static String CONTEXT_KEY_SBI_BIRT_RUNTIME_DATASET_PROXY = "SBI_BIRT_RUNTIME_DATASET_PROXY";
+    public static String BIRT_VIEWER_HTTPSERVET_REQUEST = "BIRT_VIEWER_HTTPSERVET_REQUEST";
+        
+    public static String SBI_BIRT_RUNTIME_IS_RUNTIME = "SBI_BIRT_RUNTIME_IS_RUNTIME";
+    public static String SBI_BIRT_RUNTIME_USER_ID = "SBI_BIRT_RUNTIME_USER_ID";
+    public static String SBI_BIRT_RUNTIME_SECURE_ATTRS = "SBI_BIRT_RUNTIME_SECURE_ATTRS";
+    public static String SBI_BIRT_RUNTIME_SERVICE_URL = "SBI_BIRT_RUNTIME_SERVICE_URL";
+    public static String SBI_BIRT_RUNTIME_SERVER_URL = "SBI_BIRT_RUNTIME_SERVER_URL";
+    public static String SBI_BIRT_RUNTIME_TOKEN = "SBI_BIRT_RUNTIME_TOKEN";
+    public static String SBI_BIRT_RUNTIME_PASS = "SBI_BIRT_RUNTIME_PASS";
+    
+    private Object context = null;
+    
 	/*
 	 * @see org.eclipse.datatools.connectivity.oda.IDriver#getConnection(java.lang.String)
 	 */
 	public IConnection getConnection( String dataSourceType ) throws OdaException
 	{
-        // assumes that this driver supports only one type of data source,
-        // ignores the specified dataSourceType
+		if (isBirtRuntimeContext()) {
+			
+			DataSetServiceProxy proxyDataset = null;
+			try {
+				System.out.println("Tring to get the DataSetServiceProxy ...");
+				proxyDataset = getDataSetProxy();
+				System.out.println("DataSetServiceProxy obtained correctly: " + proxyDataset);
+			} catch (RuntimeException e) {
+				e.printStackTrace();
+				throw e;
+			}
+			return new RuntimeConnection(proxyDataset);
+		}
         return new Connection();
+	}
+	
+	private DataSetServiceProxy getDataSetProxy() {
+		if (!isBirtRuntimeContext()) {
+			throw new RuntimeException(
+					"This method must be invoked in Birt runtime context!!!");
+		}
+		try {
+			HashMap map = (HashMap) context;
+			String userId = getUserId();
+			String secureAttributes = getSecureAttrs();
+			String serviceUrlStr = getServiceUrl();
+			String spagoBiServerURL = getSpagoBIServerUrl();
+			String token = getToken();
+			String pass = getPass();
+			DataSetServiceProxy proxy = new DataSetServiceProxy(userId, secureAttributes, serviceUrlStr, spagoBiServerURL, token, pass);
+			return proxy;
+		} catch (Exception e) {
+			throw new RuntimeException("Error while getting DataSetServiceProxy from Birt runtime context", e);
+		}
+	}
+	
+	private String getUserId() {
+		if (!isBirtRuntimeContext()) {
+			throw new RuntimeException("This method must be invoked in Birt runtime context!!!");
+		}
+		try {
+		    HashMap map = (HashMap) context;
+		    String userId = (String) map.get(SBI_BIRT_RUNTIME_USER_ID);
+		    return userId;
+		} catch (Exception e) {
+			throw new RuntimeException("Error while getting user id from Birt runtime context", e);
+		}
+	}
+	
+	private String getSecureAttrs() {
+		if (!isBirtRuntimeContext()) {
+			throw new RuntimeException("This method must be invoked in Birt runtime context!!!");
+		}
+		try {
+		    HashMap map = (HashMap) context;
+		    String secureAttributes = (String) map.get(SBI_BIRT_RUNTIME_SECURE_ATTRS);
+		    return secureAttributes;
+		} catch (Exception e) {
+			throw new RuntimeException("Error while getting user id from Birt runtime context", e);
+		}
+	}
+	
+	private String getServiceUrl() {
+		if (!isBirtRuntimeContext()) {
+			throw new RuntimeException("This method must be invoked in Birt runtime context!!!");
+		}
+		try {
+		    HashMap map = (HashMap) context;
+		    String serviceUrlStr = (String) map.get(SBI_BIRT_RUNTIME_SERVICE_URL);
+		    return serviceUrlStr;
+		} catch (Exception e) {
+			throw new RuntimeException("Error while getting user id from Birt runtime context", e);
+		}
+	}
+	
+	private String getSpagoBIServerUrl() {
+		if (!isBirtRuntimeContext()) {
+			throw new RuntimeException("This method must be invoked in Birt runtime context!!!");
+		}
+		try {
+		    HashMap map = (HashMap) context;
+		    String spagoBiServerURL = (String) map.get(SBI_BIRT_RUNTIME_SERVER_URL);
+		    return spagoBiServerURL;
+		} catch (Exception e) {
+			throw new RuntimeException("Error while getting user id from Birt runtime context", e);
+		}
+	}
+	
+	private String getToken() {
+		if (!isBirtRuntimeContext()) {
+			throw new RuntimeException("This method must be invoked in Birt runtime context!!!");
+		}
+		try {
+		    HashMap map = (HashMap) context;
+		    String token = (String) map.get(SBI_BIRT_RUNTIME_TOKEN);
+		    return token;
+		} catch (Exception e) {
+			throw new RuntimeException("Error while getting user id from Birt runtime context", e);
+		}
+	}
+	
+	private String getPass() {
+		if (!isBirtRuntimeContext()) {
+			throw new RuntimeException("This method must be invoked in Birt runtime context!!!");
+		}
+		try {
+		    HashMap map = (HashMap) context;
+		    String pass = (String) map.get(SBI_BIRT_RUNTIME_PASS);
+		    return pass;
+		} catch (Exception e) {
+			throw new RuntimeException("Error while getting user id from Birt runtime context", e);
+		}
+	}
+
+	private boolean isBirtRuntimeContext() {
+		System.out.println("Entering isBirtRuntimeContext");
+	    if (context != null && context instanceof HashMap) {
+	    	HashMap map = (HashMap) context;
+	    	String isRuntime = (String) map.get(SBI_BIRT_RUNTIME_IS_RUNTIME);
+	    	if(isRuntime!=null && isRuntime.equals("true")){
+	    		System.out.println("Ok runtime");
+	    		return true;	
+	    	}else{
+	    		System.out.println("NOT runtime");
+	    		return false;
+	    	}
+	    }
+	    return false;
 	}
 
 	/*
@@ -54,7 +201,22 @@ public class Driver implements IDriver
 	 */
 	public void setAppContext( Object context ) throws OdaException
 	{
-	    // do nothing; assumes no support for pass-through context
+		this.context = context;
+	    System.out.println("Driver: start setAppContext");
+	    System.out.println(context != null ? context.getClass().getCanonicalName() : "null");
+	    if (context != null && context instanceof HashMap) {
+	    	HashMap map = (HashMap) context;
+	    	EngineConstants d = null;
+	    	Set<Map.Entry> entries = map.entrySet();
+	    	Iterator<Map.Entry> it = entries.iterator();
+	    	while (it.hasNext()) {
+	    		Map.Entry entry = it.next();
+	    		Object key = entry.getKey();
+	    		Object value = entry.getValue();
+	    		System.out.println("Entry key [" + key + "], value [" + value + "]");
+	    	}
+	    }
+	    System.out.println("Driver: end setAppContext");
 	}
 
     /**
