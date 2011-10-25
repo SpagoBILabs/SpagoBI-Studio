91c3e30797f6e0cc9877d8797efde5167295b7e2
Giulio Gavardi <giulio.gavardi@eng.it>
2011-10-25 14:42:22 +0000

    added cfield.xml upload
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Studio/trunk@16657 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- it.eng.spagobi.studio.core/src/it/eng/spagobi/studio/core/services/datamartTemplate/UploadDatamartTemplateService.java
+++ it.eng.spagobi.studio.core/src/it/eng/spagobi/studio/core/services/datamartTemplate/UploadDatamartTemplateService.java
@@ -8,6 +8,8 @@ import it.eng.spagobi.studio.utils.bo.Template;
 import it.eng.spagobi.studio.utils.exceptions.NoActiveServerException;
 import it.eng.spagobi.studio.utils.services.SpagoBIServerObjectsFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.rmi.RemoteException;
 
@@ -34,6 +36,10 @@ public class UploadDatamartTemplateService {
 	private static Logger logger = LoggerFactory.getLogger(UploadDatamartTemplateService.class);
 	String projectname = null;
 	String modelname = null;
+
+
+	public static final String DATAMART_JAR = "datamart.jar";
+	public static final String CALCULATED_FIELD = "cfields.xml";
 
 	public UploadDatamartTemplateService(ISelection _selection) {
 		selection = _selection;	
@@ -70,7 +76,7 @@ public class UploadDatamartTemplateService {
 		}
 		final BusinessModel finalBusinessModel = businessModel;
 		final File finalBusinessModelFile = fileSel ; 
-		
+
 		// generate the jar
 
 
@@ -83,7 +89,7 @@ public class UploadDatamartTemplateService {
 		if(!tempDirPath.endsWith(java.io.File.separator)){
 			tempDirPath += java.io.File.separator;
 		}
-		
+
 		String idFolderPath = businessModel.getName()+"_"+UUID;
 		String tempDirPathId = tempDirPath + idFolderPath;
 		logger.debug("create model in temporary folder "+tempDirPathId);
@@ -100,36 +106,10 @@ public class UploadDatamartTemplateService {
 
 
 		// search for file datamart.jar, is rooted in the folder created
-
-		String pathToSearch = tempDirPathId + java.io.File.separator + businessModel.getName() + java.io.File.separator + "dist"+java.io.File.separator +"datamart.jar";
-		logger.debug("try reatrieving file "+pathToSearch);
+		String pathToSearch = tempDirPathId + java.io.File.separator + businessModel.getName() + java.io.File.separator + "dist"+java.io.File.separator +DATAMART_JAR;
+		logger.debug("try reatrieving datamart.jar file "+pathToSearch);
 		Path tmppath = new Path(pathToSearch);
-
 		java.io.File datamart = tmppath.toFile();
-
-		//		if(datamart.exists()){
-		//			if(folder.getFolder(businessModel.getName()).exists()){
-		//				IFolder modelNameFolder = folder.getFolder(businessModel.getName());
-		//				if(modelNameFolder.getFolder("dist").exists()){
-		//					logger.debug("Entered in "+modelNameFolder.getFullPath().toOSString());
-		//					IFolder fold = modelNameFolder.getFolder("dist");
-		//					// search for datamart.jar
-		//					if(fold.getFile("datamart.jar").exists()){
-		//						datamartFile = fold.getFile("datamart.jar");
-		//					}
-		//					else
-		//						errorMessage = "Could not find datamart.jar in "+modelNameFolder.getFullPath().toOSString()+"/dist/";
-		//				}
-		//				else
-		//					errorMessage = "Could not find dist folder on "+folder.getFullPath().toOSString()+java.io.File.separator+businessModel.getName()+"/dist/ ";	
-		//			}
-		//			else
-		//				errorMessage = "Could not find dist folder on "+folder.getFullPath().toOSString()+java.io.File.separator+businessModel.getName()+"/ ";							
-		//		}
-		//		else{
-		//			errorMessage = "Could not find folder "+folder.getFullPath().toOSString();				
-		//		}
-
 		if(datamart == null){
 			logger.error("could not retrieve file "+pathToSearch);
 			MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Error",
@@ -139,14 +119,30 @@ public class UploadDatamartTemplateService {
 		else{
 			logger.debug("found file "+businessModel.getName()+"/dist/datamart.jar");
 		}
-		
+
+		// search for non mandatory file calculatedFields, rooted in the folder created too
+		String pathToSearchXml = tempDirPathId + java.io.File.separator + businessModel.getName() + java.io.File.separator + "dist"+java.io.File.separator +CALCULATED_FIELD;
+		logger.debug("try reatrieving calculatedFields xml file "+pathToSearch);
+		Path tmppathXml = new Path(pathToSearchXml);
+		java.io.File xmlFile = tmppathXml.toFile();
+		if(xmlFile == null || !xmlFile.exists()){
+			logger.warn("Xml file for calculate dields was not found in "+pathToSearchXml);
+			xmlFile = null;
+		}
+		else{
+			logger.debug("found file for calculate dfields in "+pathToSearchXml+"/dist/datamart.jar");
+		}		
+
+
+
 		// got the template datamart.jar
 		final java.io.File finalDatamartFile = datamart;
+		final java.io.File finalXmlFile = xmlFile;
 		final NoActiveServerException noActiveServerException=new NoActiveServerException();
 
 		IRunnableWithProgress op = new IRunnableWithProgress() {			
 			public void run(IProgressMonitor monitor) throws InvocationTargetException {
-				monitor.beginTask("Deploying model files (datamart.jar and " + modelname +")", IProgressMonitor.UNKNOWN);
+				monitor.beginTask("Deploying model files (datamart.jar and xmlFile for calculated fields " + modelname +")", IProgressMonitor.UNKNOWN);
 
 				SpagoBIServerObjectsFactory spagoBIServerObjects = null;
 				try{
@@ -160,7 +156,8 @@ public class UploadDatamartTemplateService {
 
 				Template datamartTemplate = new Template();
 				Template modelTemplate = new Template();
-				
+				Template xmlCalcFieldsTemplate = new Template();
+
 				//defines properties for datamart file
 				datamartTemplate.setFileName(finalDatamartFile.getName());
 				datamartTemplate.setFolderName(finalBusinessModel.getName());
@@ -169,10 +166,39 @@ public class UploadDatamartTemplateService {
 				DataHandler dataHandler=new DataHandler(fileDataSource);			
 				datamartTemplate.setContent(dataHandler);
 				logger.debug("built Datamart template with content data handler");
-				
+
 				//defines properties for sbimodel file 
 				modelTemplate.setFileName(modelname);
 				modelTemplate.setFolderName(finalBusinessModel.getName());
+
+
+				// defines properties for xml Calculated fields file
+				if(finalXmlFile != null){
+					xmlCalcFieldsTemplate.setFileName(finalXmlFile.getName());
+					xmlCalcFieldsTemplate.setFolderName(finalBusinessModel.getName());
+					// create templates content
+					FileDataSource xmlDataSource=new FileDataSource(finalXmlFile);
+					DataHandler xmlDataHandler=new DataHandler(xmlDataSource);
+					
+					/* ----------- test code ---------	
+					InputStream is;
+					try {
+						is = xmlDataHandler.getInputStream();
+
+					byte[] templateContent = getByteArrayFromInputStream(is);
+					String ss = new String(templateContent);
+					System.out.println(ss);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					----------- test code --------- */	
+					
+					
+					xmlCalcFieldsTemplate.setContent(xmlDataHandler);
+					logger.debug("built xml calculated fields with content data handler");
+				}
+
 				// create templates content
 				try{
 					ByteArrayDataSource byteDataSource = new ByteArrayDataSource(finalBusinessModelFile.getContents(),"application/octet-stream");	
@@ -186,7 +212,7 @@ public class UploadDatamartTemplateService {
 
 
 				try {
-					spagoBIServerObjects.getServerDocuments().uploadDatamartTemplate(datamartTemplate);
+					spagoBIServerObjects.getServerDocuments().uploadDatamartTemplate(datamartTemplate, xmlCalcFieldsTemplate);
 				} catch (RemoteException e2) {
 					logger.error("error in uploading datamart",e2);
 					throw new InvocationTargetException(e2);
@@ -249,6 +275,31 @@ public class UploadDatamartTemplateService {
 
 	}
 
+	public static byte[] getByteArrayFromInputStream(InputStream is) {
+		logger.debug("IN");
+		try {
+			java.io.ByteArrayOutputStream baos = new java.io.ByteArrayOutputStream();
+			java.io.BufferedOutputStream bos = new java.io.BufferedOutputStream(baos);
 
+			int c = 0;
+			byte[] b = new byte[1024];
+			while ((c = is.read(b)) != -1) {
+				if (c == 1024)
+					bos.write(b);
+				else
+					bos.write(b, 0, c);
+			}
+			bos.flush();
+			byte[] ret = baos.toByteArray();
+			bos.close();
+			return ret;
+		} catch (IOException ioe) {
+			logger.error("IOException", ioe);
+			return null;
+		} finally {
+			logger.debug("OUT");
+		}
+
+	}
 
 }
