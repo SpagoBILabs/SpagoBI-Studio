e01a1d2022666c6075edcfa154092ee5fda3b102
Giulio Gavardi <giulio.gavardi@eng.it>
2011-11-10 14:52:51 +0000

    asking datasource before uploading model
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Studio/trunk@16744 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- it.eng.spagobi.studio.core/src/it/eng/spagobi/studio/core/services/datamartTemplate/UploadDatamartTemplateService.java
+++ it.eng.spagobi.studio.core/src/it/eng/spagobi/studio/core/services/datamartTemplate/UploadDatamartTemplateService.java
@@ -4,6 +4,9 @@ import it.eng.spagobi.meta.model.Model;
 import it.eng.spagobi.meta.model.business.BusinessModel;
 import it.eng.spagobi.meta.model.serializer.EmfXmiSerializer;
 import it.eng.spagobi.meta.querybuilder.ui.editor.SpagoBIDataSetEditor;
+import it.eng.spagobi.studio.core.util.ComboSelectionDialog;
+import it.eng.spagobi.studio.utils.bo.DataSource;
+import it.eng.spagobi.studio.utils.bo.Document;
 import it.eng.spagobi.studio.utils.bo.Template;
 import it.eng.spagobi.studio.utils.exceptions.NoActiveServerException;
 import it.eng.spagobi.studio.utils.services.SpagoBIServerObjectsFactory;
@@ -12,6 +15,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
@@ -28,6 +34,8 @@ import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.internal.theme.ComboDrawData;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,6 +48,10 @@ public class UploadDatamartTemplateService {
 	String projectname = null;
 	String modelname = null;
 
+	DataSource[] dataSources = null;
+	String userDataSource = null;
+	String messageStatusDocument = "";
+	boolean documentAlreadyPresent = false;
 
 	public static final String DATAMART_JAR = "datamart.jar";
 	public static final String CALCULATED_FIELD = "cfields_meta.xml";
@@ -65,7 +77,7 @@ public class UploadDatamartTemplateService {
 
 		// refresh metadata_model folder
 		refreshModelFolder(fileSel);
-		
+
 		EmfXmiSerializer emfXmiSerializer = new EmfXmiSerializer();
 
 		Model root = null;
@@ -82,8 +94,7 @@ public class UploadDatamartTemplateService {
 			"error in retrieving business model: try refreshing model folder");				
 			return false;
 		}
-		final BusinessModel finalBusinessModel = businessModel;
-		final File finalBusinessModelFile = fileSel ; 
+
 
 		// generate the jar
 
@@ -103,7 +114,7 @@ public class UploadDatamartTemplateService {
 		logger.debug("create model in temporary folder "+tempDirPathId);
 
 		try{
-			new SpagoBIDataSetEditor().generateMapping(finalBusinessModel, tempDirPathId, null);
+			new SpagoBIDataSetEditor().generateMapping(businessModel, tempDirPathId, null);
 		}
 		catch (Exception e) {
 			logger.error("Error in generating the datamart for model "+businessModel.getName(),e);
@@ -142,110 +153,68 @@ public class UploadDatamartTemplateService {
 		}		
 
 
+		SpagoBIServerObjectsFactory spagoBIServerObjects = null;
+		try{
+			spagoBIServerObjects = new SpagoBIServerObjectsFactory(projectname);
+		}
+		catch (NoActiveServerException e) {
+			logger.error("No server is defined active");			
+			MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), 
+					"Error", "No server is defined active");
+		}
 
-		// got the template datamart.jar
-		final java.io.File finalDatamartFile = datamart;
-		final java.io.File finalXmlFile = xmlFile;
+//		final BusinessModel finalBusinessModel = businessModel;
+//		final File finalBusinessModelFile = fileSel ; 
+//		final java.io.File finalDatamartFile = datamart;
+//		final java.io.File finalXmlFile = xmlFile;
 		final NoActiveServerException noActiveServerException=new NoActiveServerException();
+		final SpagoBIServerObjectsFactory spagoBIServerObjectsFinal = spagoBIServerObjects; 
 
-		IRunnableWithProgress op = new IRunnableWithProgress() {			
-			public void run(IProgressMonitor monitor) throws InvocationTargetException {
-				monitor.beginTask("Deploying model files (datamart.jar and xmlFile for calculated fields " + modelname +")", IProgressMonitor.UNKNOWN);
-
-				SpagoBIServerObjectsFactory spagoBIServerObjects = null;
-				try{
-					spagoBIServerObjects = new SpagoBIServerObjectsFactory(projectname);
-				}
-				catch (NoActiveServerException e) {
-					logger.error("no active server found");
-					noActiveServerException.setNoServer(true);
-					return;
-				}
-
-				Template datamartTemplate = new Template();
-				Template modelTemplate = new Template();
-				Template xmlCalcFieldsTemplate = new Template();
-
-				//defines properties for datamart file
-				datamartTemplate.setFileName(finalDatamartFile.getName());
-				datamartTemplate.setFolderName(finalBusinessModel.getName());
-				// create templates content
-				FileDataSource fileDataSource=new FileDataSource(finalDatamartFile);
-				DataHandler dataHandler=new DataHandler(fileDataSource);			
-				datamartTemplate.setContent(dataHandler);
-				logger.debug("built Datamart template with content data handler");
-
-				//defines properties for sbimodel file 
-				modelTemplate.setFileName(modelname);
-				modelTemplate.setFolderName(finalBusinessModel.getName());
-
-
-				// defines properties for xml Calculated fields file
-				if(finalXmlFile != null){
-					xmlCalcFieldsTemplate.setFileName(finalXmlFile.getName());
-					xmlCalcFieldsTemplate.setFolderName(finalBusinessModel.getName());
-					// create templates content
-					FileDataSource xmlDataSource=new FileDataSource(finalXmlFile);
-					DataHandler xmlDataHandler=new DataHandler(xmlDataSource);
-
-					/* ----------- test code ---------	
-					InputStream is;
-					try {
-						is = xmlDataHandler.getInputStream();
-
-					byte[] templateContent = getByteArrayFromInputStream(is);
-					String ss = new String(templateContent);
-					System.out.println(ss);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					----------- test code --------- */	
-
-
-					xmlCalcFieldsTemplate.setContent(xmlDataHandler);
-					logger.debug("built xml calculated fields with content data handler");
-				}
-
-				// create templates content
-				try{
-					ByteArrayDataSource byteDataSource = new ByteArrayDataSource(finalBusinessModelFile.getContents(),"application/octet-stream");	
-					dataHandler = new DataHandler(byteDataSource);
-					modelTemplate.setContent(dataHandler);
-					logger.debug("built Model template with content data handler");
-				}catch(Exception e){
-					logger.error("error in getting model template",e);
-					throw new InvocationTargetException(e);
-				}
-
-
-				try {
-					spagoBIServerObjects.getServerDocuments().uploadDatamartTemplate(datamartTemplate, xmlCalcFieldsTemplate);
-				}
-
-				catch (RemoteException e2) {
-					logger.error("error in uploading datamart",e2);
-					throw new InvocationTargetException(e2);
-				}
-				try {
-					spagoBIServerObjects.getServerDocuments().uploadDatamartModel(modelTemplate);
-				} catch (RemoteException e3) {
-					logger.error("error in uploading model file",e3);
-					throw new InvocationTargetException(e3);
-				}
-
-				monitor.done();
-				if (monitor.isCanceled())
-					logger.error("Operation not ended",new InterruptedException("The long running operation was cancelled"));
-			}
-		};
-
+		IRunnableWithProgress monitorCheckExistance = getMonitorCheckExistance(businessModel, spagoBIServerObjects);
+		IRunnableWithProgress monitorForDatasources = getMonitorForDatasources(businessModel, spagoBIServerObjects);
+		IRunnableWithProgress monitorForUpload = getMonitorForUpload(businessModel, spagoBIServerObjects, datamart, xmlFile, fileSel);
 
 
 		// Start monitor for upload operation
 		ProgressMonitorDialog dialog=new ProgressMonitorDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());		
+
 		try {
-			dialog.run(true, true, op);
+
+			// the dataSource should be asked only if there is not already a documetn present with datamartnamein system
+
+			// check if document is already present
+			dialog.run(true, true, monitorCheckExistance);
+
+			if(!documentAlreadyPresent){
+				// get datasources
+				dialog.run(true, true, monitorForDatasources);
+
+				// ask datasource to user			
+				if(dataSources != null){
+					logger.debug("found "+dataSources.length+ " datasources: make user choose one");
+					Map<String, DataSource> mapLabelToDatasource = new HashMap<String, DataSource> ();
+					int size = dataSources.length;
+					String[] optionsArray = new String[size];
+					for (int i = 0; i < dataSources.length; i++) {
+						DataSource ds = dataSources[i];
+						mapLabelToDatasource.put(ds.getLabel(), ds);
+						optionsArray[i] = ds.getLabel();
+					}
+
+					final Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+					ComboSelectionDialog csd = new ComboSelectionDialog(shell);	
+					csd.setMessage("Select data source for QBE document (optional)");
+					csd.setText("Data source Selection");
+					csd.setOptions(optionsArray);
+					logger.debug("Open datasource selection dialog");
+					userDataSource = csd.open();
+					logger.debug("user selected dataSource "+userDataSource);
+
+				}
+			}
+
+			// do the uploads
+			dialog.run(true, true, monitorForUpload);
 		} 
 		catch (InvocationTargetException e1) {
 			logger.error("error in uploading datamart",e1);
@@ -272,7 +241,7 @@ public class UploadDatamartTemplateService {
 		}
 
 		// if here success
-		MessageDialog.openInformation(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),"Upload succesfull", "Uploaded to resources");		
+		MessageDialog.openInformation(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),"Upload succesfull", "Succesfully uploaded to resources\n\n"+messageStatusDocument);		
 		logger.debug("Uploaded to resources in "+businessModel.getName());		
 
 		// delete the temporary file
@@ -291,11 +260,148 @@ public class UploadDatamartTemplateService {
 		logger.debug("OUT");
 		return true;
 
+
 	}
-	
-	
-	
-	
+
+
+
+	// *******  Runnable block for checking if dopcumetnAlready exists ***********		
+	IRunnableWithProgress getMonitorCheckExistance(final BusinessModel businessModel, final SpagoBIServerObjectsFactory spagoBIServerObjects ){
+		IRunnableWithProgress opCheck = new IRunnableWithProgress() {			
+			public void run(IProgressMonitor monitor) throws InvocationTargetException {
+				monitor.beginTask("check if there is already document with name "+businessModel.getName()+" ", IProgressMonitor.UNKNOWN);
+				String modelname = businessModel.getName();
+
+
+				logger.debug("is there documetn with llabel "+modelname);	
+				try{
+
+					Document doc  = spagoBIServerObjects.getServerDocuments().getDocumentByLabel(modelname);
+
+					if(doc != null){
+						documentAlreadyPresent = true;
+						messageStatusDocument = "Detail: QBEDocument with label "+modelname+" was not added because already present in server";
+					}
+					else{
+						documentAlreadyPresent = false;
+					}
+				}
+				catch (RemoteException e2) {
+					logger.error("error in uploading datamart",e2);
+					throw new InvocationTargetException(e2);
+				}
+			}
+		};
+		return opCheck;
+	}
+
+	// *******  Runnable block for retrieving datasources ***********	
+	IRunnableWithProgress getMonitorForDatasources(final BusinessModel businessModel, final SpagoBIServerObjectsFactory spagoBIServerObjects){
+		IRunnableWithProgress opDs = new IRunnableWithProgress() {			
+			public void run(IProgressMonitor monitor) throws InvocationTargetException {
+				monitor.beginTask("Retrieving data sources for selection)", IProgressMonitor.UNKNOWN);
+
+				logger.debug("get Datasources defined");
+				try{
+					UploadDatamartTemplateService.this.dataSources = spagoBIServerObjects.getServerDataSources().getDataSourceList();
+				}
+				catch (RemoteException e2) {
+					logger.error("error in uploading datamart",e2);
+					throw new InvocationTargetException(e2);
+				}
+			}
+		};
+		return opDs;
+	}
+
+
+	// *******  Runnable block for upload     ***********	
+	IRunnableWithProgress getMonitorForUpload(
+			final BusinessModel businessModel
+			, final SpagoBIServerObjectsFactory spagoBIServerObjects
+			, final java.io.File datamartFile
+			, final java.io.File xmlFile
+			, final File businessModelFile)
+	{
+		IRunnableWithProgress op = new IRunnableWithProgress() {			
+			public void run(IProgressMonitor monitor) throws InvocationTargetException {
+				monitor.beginTask("Deploying model files (datamart.jar and xmlFile for calculated fields " + modelname +")", IProgressMonitor.UNKNOWN);
+
+				Template datamartTemplate = new Template();
+				Template modelTemplate = new Template();
+				Template xmlCalcFieldsTemplate = new Template();
+
+				//defines properties for datamart file
+				datamartTemplate.setFileName(datamartFile.getName());
+				datamartTemplate.setFolderName(businessModel.getName());
+				// create templates content
+				FileDataSource fileDataSource=new FileDataSource(datamartFile);
+				DataHandler dataHandler=new DataHandler(fileDataSource);			
+				datamartTemplate.setContent(dataHandler);
+				logger.debug("built Datamart template with content data handler");
+
+				//defines properties for sbimodel file 
+				modelTemplate.setFileName(modelname);
+				modelTemplate.setFolderName(businessModel.getName());
+
+
+				// defines properties for xml Calculated fields file
+				if(xmlFile != null){
+					xmlCalcFieldsTemplate.setFileName(xmlFile.getName());
+					xmlCalcFieldsTemplate.setFolderName(businessModel.getName());
+					// create templates content
+					FileDataSource xmlDataSource=new FileDataSource(xmlFile);
+					DataHandler xmlDataHandler=new DataHandler(xmlDataSource);
+
+					xmlCalcFieldsTemplate.setContent(xmlDataHandler);
+					logger.debug("built xml calculated fields with content data handler");
+				}
+
+				// create templates content
+				try{
+					ByteArrayDataSource byteDataSource = new ByteArrayDataSource(businessModelFile.getContents(),"application/octet-stream");	
+					dataHandler = new DataHandler(byteDataSource);
+					modelTemplate.setContent(dataHandler);
+					logger.debug("built Model template with content data handler");
+				}catch(Exception e){
+					logger.error("error in getting model template",e);
+					throw new InvocationTargetException(e);
+				}
+
+
+				try {
+					spagoBIServerObjects.getServerDocuments().uploadDatamartTemplate(datamartTemplate, xmlCalcFieldsTemplate, userDataSource);
+				}
+
+				catch (RemoteException e2) {
+					logger.error("error in uploading datamart",e2);
+					throw new InvocationTargetException(e2);
+				}
+				try {
+					spagoBIServerObjects.getServerDocuments().uploadDatamartModel(modelTemplate);
+				} catch (RemoteException e3) {
+					logger.error("error in uploading model file",e3);
+					throw new InvocationTargetException(e3);
+				}
+
+				if(documentAlreadyPresent == false){
+					messageStatusDocument = "Detail: QBE Document with label "+modelname+ " added to server";
+				}
+				monitor.done();
+				if (monitor.isCanceled())
+					logger.error("Operation not ended",new InterruptedException("The long running operation was cancelled"));
+			}
+		};
+		return op;
+
+	}
+
+
+
+
+
+
+
 	void refreshModelFolder(File file){
 		logger.debug("IN");
 		// search for "Metadata_Model" folder to refresh
@@ -306,7 +412,7 @@ public class UploadDatamartTemplateService {
 		while(folder != null && !folder.getName().equals(METADATA_MODEL_FOLDER)){
 			folder = folder.getParent();	
 		}
-		
+
 		if(folder != null){
 			try {
 				folder.refreshLocal(IResource.DEPTH_INFINITE, null);
@@ -319,9 +425,9 @@ public class UploadDatamartTemplateService {
 		logger.debug("OUT");
 
 	}
-	
-	
-	
+
+
+
 
 	public static byte[] getByteArrayFromInputStream(InputStream is) {
 		logger.debug("IN");
---
+++ it.eng.spagobi.studio.core/src/it/eng/spagobi/studio/core/util/ComboSelectionDialog.java
@@ -0,0 +1,217 @@
+package it.eng.spagobi.studio.core.util;
+
+//Send questions, comments, bug reports, etc. to the authors:
+
+//Rob Warner (rwarner@interspatial.com)
+//Robert Harris (rbrt_harris@yahoo.com)
+
+import java.util.Arrays;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Monitor;
+import org.eclipse.swt.widgets.Shell;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class takes a map of <label,object> and makes a dialog to let user choose among these
+ */
+public class ComboSelectionDialog extends Dialog {
+	private String input;
+	private String message;
+	private String label;
+
+	private Combo optionsCombo; 
+
+	String[] options = null;
+
+	private static Logger logger = LoggerFactory.getLogger(ComboSelectionDialog.class);
+
+	/**
+	 * InputDialog constructor
+	 * 
+	 * @param parent the parent
+	 */
+	public ComboSelectionDialog(Shell parent) {
+		// Pass the default styles here
+		this(parent, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+
+	}
+
+	/**
+	 * InputDialog constructor
+	 * 
+	 * @param parent the parent
+	 * @param style the style
+	 */
+	public ComboSelectionDialog(Shell parent, int style) {
+		// Let users override the default styles
+		super(parent, style);
+		setText("Input Dialog");
+		setMessage("Please enter a value:");
+	}
+
+	/**
+	 * Gets the message
+	 * 
+	 * @return String
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * Sets the message
+	 * 
+	 * @param message the new message
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * Gets the input
+	 * 
+	 * @return String
+	 */
+	public String getInput() {
+		return input;
+	}
+
+	/**
+	 * Sets the input
+	 * 
+	 * @param input the new input
+	 */
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+
+
+
+
+	public String[] getOptions() {
+		return options;
+	}
+
+	public void setOptions(String[] options) {
+		this.options = options;
+	}
+
+
+
+	/**
+	 * Opens the dialog and returns the input
+	 * 
+	 * @return String
+	 */
+	public String open() {
+		// Create the dialog window
+		Shell shell = new Shell(getParent(), getStyle());
+		shell.setText(getText());
+
+		shell.setSize(200, 200);
+		Display display = shell.getDisplay();
+		Monitor primary = display.getPrimaryMonitor();
+		Rectangle bounds = primary.getBounds();
+		Rectangle rect = shell.getBounds();
+		int x = bounds.x + (bounds.width - rect.width) / 2;
+		int y = bounds.y + (bounds.height - rect.height) / 2;
+		shell.setLocation(x, y);
+
+		createContents(shell);
+		shell.pack();
+		shell.open();
+		Display displayParent = getParent().getDisplay();
+		while (!shell.isDisposed()) {
+			if (!displayParent.readAndDispatch()) {
+				displayParent.sleep();
+			}
+		}
+		// Return the entered value, or null
+		return input;
+	}
+
+	/**
+	 * Creates the dialog's contents
+	 * 
+	 * @param shell the dialog window
+	 */
+	private void createContents(final Shell shell) {
+		logger.debug("IN");
+		shell.setLayout(new GridLayout(2, true));
+
+		new Label(shell, SWT.NONE).setText("");
+		new Label(shell, SWT.NONE).setText("");
+
+		// Show the message
+		Label label = new Label(shell, SWT.NONE);
+		label.setText(message);
+		GridData data = new GridData();
+		data.horizontalSpan = 2;
+		label.setLayoutData(data);
+		optionsCombo = new Combo(shell, SWT.NONE | SWT.READ_ONLY);
+		optionsCombo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+
+		Arrays.sort(options);
+		optionsCombo.setItems(options);
+
+		new Label(shell, SWT.NONE).setText("");
+		new Label(shell, SWT.NONE).setText("");
+		new Label(shell, SWT.NONE).setText("");
+		new Label(shell, SWT.NONE).setText("");
+
+		// Create the OK button and add a handler
+		// so that pressing it will set input
+		// to the entered value
+		Button ok = new Button(shell, SWT.PUSH);
+		ok.setText("Deploy");
+		data = new GridData(GridData.FILL_HORIZONTAL);
+		ok.setLayoutData(data);
+		ok.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				int index = optionsCombo.getSelectionIndex();
+				if(index != -1) 
+					input = optionsCombo.getItem(index);
+				else input = null;
+				shell.close();
+			}
+		});
+
+		//		// Create the cancel button and add a handler
+		//		// so that pressing it will set input to null
+		//		Button cancel = new Button(shell, SWT.PUSH);
+		//		cancel.setText("Cancel");
+		//		data = new GridData(GridData.FILL_HORIZONTAL);
+		//		cancel.setLayoutData(data);
+		//		cancel.addSelectionListener(new SelectionAdapter() {
+		//			public void widgetSelected(SelectionEvent event) {
+		//				input = null;
+		//				shell.close();
+		//			}
+		//		});
+
+		// Set the OK button as the default, so
+		// user can type input and press Enter
+		// to dismiss
+		shell.setDefaultButton(ok);
+		logger.debug("OUT");
+	}
+
+
+}
+
+
+
