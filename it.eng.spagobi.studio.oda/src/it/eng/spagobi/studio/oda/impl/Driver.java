5cd020addf3540f7e9aced2c8eb3de257be17b61
Marco Cortella <macortel@eng.it>
2012-06-27 10:55:04 +0000

    added license header for MPL 2.0 license
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_3.x/Studio/trunk@18372 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- it.eng.spagobi.studio.oda/src/it/eng/spagobi/studio/oda/impl/Driver.java
+++ it.eng.spagobi.studio.oda/src/it/eng/spagobi/studio/oda/impl/Driver.java
@@ -1,9 +1,12 @@
-/*
- *************************************************************************
- * Copyright (c) 2008 <<Your Company Name here>>
- *  
- *************************************************************************
- */
+/**
+ SpagoBI, the Open Source Business Intelligence suite
+
+ Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ You can obtain one at http://mozilla.org/MPL/2.0/.
+ 
+**/
 
 package it.eng.spagobi.studio.oda.impl;
 
